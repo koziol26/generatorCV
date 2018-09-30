f0304ac4c2ead44878fa037c746ac160b8bc9d6b
koziol26 <kozlow3@o2.pl>
2018-09-30 12:38:27 +0200

    second commit

---
+++ src/main/java/pl/akademiakodu/generatorCV/GeneratorCvApplication.java
@@ -0,0 +1,12 @@
+package pl.akademiakodu.generatorCV;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeneratorCvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeneratorCvApplication.class, args);
+	}
+}
